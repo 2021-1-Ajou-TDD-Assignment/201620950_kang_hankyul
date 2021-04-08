ccf34dbfd91b7453b028f25d96f68932788e9fd9
hankyul <consistant1y@ajou.ac.kr>
2021-04-08 16:55:45 +0900

    Refactor : The Third test case
    - rollSpare to make spare
    - isSpare for check spare

--- BowlingGame/test/BowlingGameTest.java
+++ BowlingGame/test/BowlingGameTest.java
@@ -32,10 +32,14 @@ public class BowlingGameTest {
 	
 	@Test
 	public void testOneSpare() {
-		g.roll(5);
-		g.roll(5); // spare
+		rollSpare();
 		g.roll(3);
 		rollMany(17, 0);
 		assertEquals(16, g.score());
+	}
+	
+	public void rollSpare() {
+		g.roll(5);
+		g.roll(5);
 	}
 }
